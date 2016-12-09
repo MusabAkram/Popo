396737361fb8ff7e4ec54467a1fc2e4e9e55647c
MusabAkram <musabakram16@gmail.com>
2016-12-09 22:17:03 +0500

    Initial commit

---
+++ app/src/main/java/com/example/musab/popo/Popo.java
@@ -0,0 +1,13 @@
+package com.example.musab.popo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Popo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popo);
+    }
+}
